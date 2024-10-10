015e6a1034ff4e791842fc4fd827716faf2f62f2
Mohit Lahoti <engtechno25@gmail.com>
2024-10-11 00:27:09 +0530

    Back to http simple wdiget

--- lib/widgets/grocery_item.dart
+++ lib/widgets/grocery_item.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
@@ -14,19 +15,25 @@ class GroceryItem extends StatefulWidget {
 
 class _GroceryItemState extends State<GroceryItem> {
   List<GroceryList> _groceryItems = [];
-  late Future<List<GroceryList>> _loadedItem;
+  var _isloading = true;
+  String? _error;
 
-  Future<List<GroceryList>> _loadItem() async {
+  void _loadItem() async {
     final url = Uri.https(
         'shopping-list-flutter-d48be-default-rtdb.firebaseio.com',
         'shopping-list.json');
     final response = await http.get(url);
     if (response.statusCode >= 400) {
-      throw Exception("failed to fetch content. Please try again later....");
+      setState(() {
+        _error = "Failed to fetch content.Please try again later...";
+      });
     }
 
     if (response.body == "null") {
-      return [];
+      setState(() {
+        _isloading = false;
+      });
+      return;
     }
 
     final Map<String, dynamic> listData = json.decode(response.body);
@@ -42,12 +49,15 @@ class _GroceryItemState extends State<GroceryItem> {
           quantity: item.value['quantity'],
           category: category));
     }
-    return _loadedItems;
+    setState(() {
+      _groceryItems = _loadedItems;
+      _isloading = false;
+    });
   }
 
   @override
   void initState() {
-    _loadedItem = _loadItem();
+    _loadItem();
     super.initState();
   }
 
@@ -80,52 +90,49 @@ class _GroceryItemState extends State<GroceryItem> {
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text("No items added yet."),
+    );
+
+    if (_isloading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (_error != null) {
+      content = Center(
+        child: Text(_error!),
+      );
+    }
+
+    if (_groceryItems.isNotEmpty) {
+      setState(() {
+        content = ListView.builder(
+            itemCount: _groceryItems.length,
+            itemBuilder: (ctx, index) => Dismissible(
+                  key: ValueKey(_groceryItems[index].id),
+                  onDismissed: (direction) {
+                    _deleteItem(_groceryItems[index]);
+                  },
+                  child: ListTile(
+                    title: Text(_groceryItems[index].name),
+                    leading: Container(
+                      height: 24,
+                      width: 24,
+                      color: _groceryItems[index].category.color,
+                    ),
+                    trailing: Text(_groceryItems[index].quantity.toString()),
+                  ),
+                ));
+      });
+    }
+
     return Scaffold(
         appBar: AppBar(
           title: const Text("Your Grocieries"),
           actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add))],
         ),
-        body: FutureBuilder(
-          future: _loadedItem,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            if (snapshot.hasError) {
-              return Center(
-                child: Text(snapshot.error.toString()),
-              );
-            }
-
-            if (snapshot.data!.isEmpty) {
-              return const Center(
-                child: Text("No items added yet."),
-              );
-              ;
-            }
-
-            return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (ctx, index) => Dismissible(
-                      key: ValueKey(snapshot.data![index].id),
-                      onDismissed: (direction) {
-                        _deleteItem(snapshot.data![index]);
-                      },
-                      child: ListTile(
-                        title: Text(snapshot.data![index].name),
-                        leading: Container(
-                          height: 24,
-                          width: 24,
-                          color: snapshot.data![index].category.color,
-                        ),
-                        trailing:
-                            Text(snapshot.data![index].quantity.toString()),
-                      ),
-                    ));
-          },
-        ));
+        body: content);
   }
 }
